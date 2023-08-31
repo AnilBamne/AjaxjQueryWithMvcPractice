50d0ca452e92794f65193ce4321299e664e6fd5a
Anil Bamne <99521630+AnilBamne@users.noreply.github.com>
2023-08-31 14:59:14 +0530

    [Anil] Add: Created_A_MVC_ProjectAndAddedSimpleCodeForShowingAnimationOnUI

---
+++ AjaxjQueryWithMvcPractice/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using AjaxjQueryWithMvcPractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxjQueryWithMvcPractice.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmpRepo empRepo;
+        public EmployeeController(EmpRepo empRepo) {
+            this.empRepo = empRepo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            var result=empRepo.Add(employee);
+            return RedirectToAction("GetAll");
+        }
+        public IActionResult GetAll()
+        {
+            var result=empRepo.GetList();
+            return View(result);
+        }
+    }
+}
---
+++ AjaxjQueryWithMvcPractice/Models/EmpRepo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AjaxjQueryWithMvcPractice.Models
+{
+    public class EmpRepo
+    {
+        //this is the repository class
+        List<Employee> list = new List<Employee> ();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
+        public string Add(Employee employee)
+        {
+            list.Add(employee);
+            return "successfull";
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Employee> GetList()
+        {
+            return list;
+        }
+    }
+}
